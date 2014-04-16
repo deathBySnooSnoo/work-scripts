7aa33fbba42a74d8322288d9ba334e9ec46cb95e
unknown <dbittner@Joker.ad.engr.wisc.edu>
2014-04-16 14:57:46 -0500

    initial commit

---
+++ install_java32.ps1
@@ -0,0 +1,43 @@
+﻿$java_version = "1.7.0_55"
+$jre_installer = "jre1.7.0_55.msi"
+$jdk_installer = "jdk1.7.0_55.msi"
+$old_jre_uninstaller = "jre1.7.0_51.msi"
+
+function install_java($java_installer){
+    if($java_installer -eq $jdk_installer){
+        $argslist = "/qn /lv C:\windows\logs\jdkmsix86.log REBOOT=Suppress"
+        $modify_registry = $false
+    }
+    else{
+        $argslist = "/qn /lv C:\windows\logs\jremsix86.log /norestart ADDLOCAL=ALL IEXPLORER=1 MOZILLA=1 REBOOT=Suppress JAVAUPDATE=0"
+        $modify_registry = $true
+    }
+    start -wait -filepath $java_installer -ArgumentList $argslist
+    if($modify_registry){
+        New-Item -Path "HKLM:\SOFTWARE\Wow6432Node\JavaSoft\Java Plug-in\$java_version"
+        New-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\JavaSoft\Java Plug-in\$java_version" -Name "HideSystemTrayIcon" -PropertyType "DWORD" -Value 1 -Force
+    }
+}
+
+if (test-path -Path "C:\Program Files (x86)\Java\jre7\bin"){
+    (start -wait -FilePath msiexec.exe -ArgumentList "/x $old_jre_uninstaller /qn").ExitCode
+    if(test-path -Path "HKLM:\SOFTWARE\Wow6432Node\JavaSoft\Java Runtime Environment"){
+        $jre_keys = get-childitem 'HKLM:\SOFTWARE\Wow6432Node\JavaSoft\Java Runtime Environment'
+        foreach($key in $jre_keys){
+            if($key -match "\b1.7.0_\d{1,}\b"){
+                $key_name = $key.Name -replace "\bHKEY_LOCAL_MACHINE", "HKLM:"
+                Remove-Item $key_name -recurse
+            }
+        }
+        $classes = Get-ChildItem HKLM:\SOFTWARE\Classes\Installer\Products
+        foreach($GUID in $classes){
+            if((Get-ItemProperty $GUID.PsPath).ProductName -match "\bJava 7 Update \d{1,}$\b"){
+                $GUID_name = $GUID.Name -replace "\bHKEY_LOCAL_MACHINE", "HKLM:"
+                Remove-Item $GUID_name -Recurse
+            }
+        }
+        (start -wait -FilePath msiexec.exe -ArgumentList "/x $old_jre_uninstaller /qn").ExitCode
+    }
+}
+install_java($jre_installer)
+install_java($jdk_installer)
