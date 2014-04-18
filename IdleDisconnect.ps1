73519697d76e1774dce82d2960a4643a5614c31d
unknown <oney@SUPERSTAR.ad.engr.wisc.edu>
2014-04-18 14:45:36 -0500

    initial commit

---
+++ IdleDisconnect.ps1
@@ -0,0 +1,16 @@
+﻿Add-PSSnapin Citrix.Broker.Admin.V2 
+$unregistered_machines = Get-BrokerDesktop -Filter { RegistrationState -eq 'Unregistered' }
+$disconnected_sessions = Get-BrokerSession -Filter { SessionState -eq 'Disconnected' -and SessionStateChangeTime -lt '-0:15' }
+$long_sessions = Get-BrokerSession -Filter { SessionState -eq 'Active' -and SessionStateChangeTime -lt '-12:00' }
+$sessions = $disconnected_sessions + $long_sessions
+foreach ($s in $sessions){
+    $registered_machine = $true
+    foreach ($machine in $unregistered_machines){
+        if($machine.MachineName.compareTo($s.MachineName) -eq 0){
+            $registered_machine = $false
+        }
+    }
+    if($registered_machine -eq $true){
+        Stop-BrokerSession -inputobject $s
+    }
+}
