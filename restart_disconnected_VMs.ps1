866321d8dd6dcd2068a1dd7c1fca9251df1c09c2
unknown <dbittner@Joker.ad.engr.wisc.edu>
2014-07-15 10:24:11 -0500

    created script

---
+++ restart_disconnected_VMs.ps1
@@ -0,0 +1,5 @@
+﻿Add-PSSnapin Citrix.Broker.Admin.V2
+$unregistered_machines = Get-BrokerDesktop -Filter { (RegistrationState -eq 'Unregistered') -and (PowerState -eq 'On') }
+foreach($machine in $unregistered_machines){
+    New-BrokerHostingPowerAction -MachineName $machine.MachineName -Action Reset
+}
