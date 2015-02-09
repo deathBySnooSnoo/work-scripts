22464b3774360973a78f8cc3aebbfaff2a669539
unknown <dbittner@Joker.ad.engr.wisc.edu>
2015-02-09 15:00:48 -0600

    changed filtering to do only CAE machines

--- IdleDisconnect.ps1
+++ IdleDisconnect.ps1
@@ -1,8 +1,9 @@
 ﻿Add-PSSnapin Citrix.Broker.Admin.V2 
 $unregistered_machines = Get-BrokerDesktop -Filter { RegistrationState -eq 'Unregistered' }
-$disconnected_sessions = Get-BrokerSession -Filter { SessionState -eq 'Disconnected' -and SessionStateChangeTime -lt '-0:15' }
-$long_sessions = Get-BrokerSession -Filter { SessionState -eq 'Active' -and SessionStateChangeTime -lt '-12:00' }
-$sessions = $disconnected_sessions + $long_sessions
+#$disconnected_sessions = Get-BrokerSession -Filter { SessionState -eq 'Disconnected' -and SessionStateChangeTime -lt '-0:15' -and HostedMachineName -contains 'xen76-cae*' }
+#$long_sessions = Get-BrokerSession -Filter { SessionState -eq 'Active' -and SessionStateChangeTime -lt '-12:00' -and HostedMachineName -contains 'xen76-cae*' }
+#$sessions = $disconnected_sessions + $long_sessions
+$sessions = Get-BrokerSession -filter { ((SessionState -eq 'Disconnected' -and SessionStateChangeTime -lt '-0:15') -or (SessionState -eq 'Active' -and SessionStateChangeTime -lt '-12:00')) -and (HostedMachineName -contains 'xen76-cae*') }
 foreach ($s in $sessions){
     $registered_machine = $true
     foreach ($machine in $unregistered_machines){
